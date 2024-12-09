f339af28bfeae26a881a3b56544e58b29bc59eee
Daniyal Hashmi <hashmid359@gmail.com>
2024-12-09 19:00:44 +0500

    Add files via upload

---
+++ types.js
@@ -0,0 +1,10 @@
+//alert(3 +3) we are using nodejs not browesr
+
+console.log(2+3); 
+
+let name  = " daniel "
+let age = 23
+let isloggedIn =  false 
+
+console.log(typeof 4349423423423423423 )
+
---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId = 1232123
+let accountEmail = "ajdashdasasj"
+var accountPaassword = "23232"
+accountcity  = " karachi"
+let accountstate ;
+// accountId = 2  not allowed 
+
+accountEmail = "bfsjfsd,com"
+accountPaassword = "23224"
+accountcity = "karachi"
+
+console.log(accountId);
+
+// prefer not to use var becaus of block scope and funtional scope 
+
+console.table([accountId,accountEmail,accountPaassword,accountcity,accountstate])
+
