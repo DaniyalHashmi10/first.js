c493a19c43d54035091e7759285dd96652fa498c
Daniyal Hashmi <hashmid359@gmail.com>
2024-12-09 20:48:43 +0500

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,23 @@
+ let value  = 3
+ let negvalue  =  -value
+//  console.log(negvalue);
+ /*
+   console.log(2%3);
+   console.log(2**2);
+   console.log(2**4);
+   
+   let strt1 = " daniel "
+   let strt3 = " hashmi"
+   let strt4 = strt1+strt3
+   console.log(strt4)
+   */
+
+// console.log("1" + 2+2)
+// console.log(1+ "2"+"2")
+// console.log(1+ 2+"2")
+
+
+// if string come first whlie  adding it some value the it will show the value separately 
+// but if adding come first string in last it wil add first then print string value  
+
+console.log(+true) // ==> 1 
