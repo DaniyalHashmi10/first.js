7785d35f67dc7d2a4aa790d45e5888287ce1b2ac
Daniyal Hashmi <hashmid359@gmail.com>
2024-12-09 19:46:50 +0500

    Add files via upload
    conversion in javascript convert any value number or string value to desired type

---
+++ conversion.js
@@ -0,0 +1,18 @@
+
+/*
+let score = "230"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinNumber =  Number (score)
+console.log(typeof valueinNumber);
+console.log( valueinNumber);
+
+*/
+
+let isloggedin = 
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
