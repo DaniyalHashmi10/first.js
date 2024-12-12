a37c1b715b31e39a6a52b4f4ee36cb7d35ec3adc
Daniyal Hashmi <hashmid359@gmail.com>
2024-12-13 00:24:21 +0500

    Add files via upload
    heap and stack touch

---
+++ practice.js
@@ -0,0 +1,16 @@
+// primitive heap and non primitve heap 
+// stack primitive 
+
+
+ let userone = {
+    email : "daniyal.com",
+    id : 231,
+    batch : "2nd"
+ }
+  let usertwo =  userone
+  usertwo.email = "dannybadshah"
+console.log(userone);
+  console.log(usertwo);
+
+// in heap it changes the original values
+// but in stack it changes in the copy 
